5bbdb33b3f8973a6e4c014b396e2e10f55931e86
Avinash Mishra <avinashm2427@gmail.com>
2020-08-20 19:34:16 +0530

    Removed email requirement during registration

--- blogsite/forms.py
+++ blogsite/forms.py
@@ -4,14 +4,12 @@ from django.contrib.auth.models import User
 from django.forms.widgets import TextInput
 
 class RegisterForm(UserCreationForm):
-
-	phone = forms.CharField(help_text = '10 digit number.')
+	
 	location = forms.CharField(max_length = 25)
 	birth_date = forms.DateField(help_text = 'Required. Format: YYYY-MM-DD')
 	username = forms.CharField(widget = TextInput(attrs = {'id' : 'username', 'placeholder' : 'username'}))
-	email = forms.CharField(widget = TextInput(attrs = {'id' : 'email', 'placeholder' : 'Email ID'}))
 
 	class Meta:
 
 		model = User 
-		fields = ('username', 'email', 'first_name', 'last_name', 'birth_date', 'password1', 'password2', )
+		fields = ('username', 'first_name', 'last_name', 'birth_date', 'password1', 'password2', )
