5a0477895c3475dd310d202567b5153a3009e76b
Avinash Mishra <avinashm2427@gmail.com>
2021-10-09 16:41:02 +0530

    Registered RatingHistory model with admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,10 +1,11 @@
 # Importing libraries 
 
 from django.contrib import admin
-from .models import QuizQuestion, Contest, Submission 
+from .models import QuizQuestion, Contest, Submission, RatingHistory
 
 # Register your models here.
 
 admin.site.register(QuizQuestion) 
 admin.site.register(Contest) 
 admin.site.register(Submission) 
+admin.site.register(RatingHistory) 
