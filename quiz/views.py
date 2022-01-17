b97d2ddfbb746b30b0faa79b2d50c449a9208066
Avinash Mishra <avinashm2427@gmail.com>
2022-01-18 00:39:24 +0530

    Showing count of people answering a question

--- quiz/views.py
+++ quiz/views.py
@@ -107,17 +107,12 @@ def homepage(request):
 			new_contests.append(contest)
 
 	# Display only those contests who have all the questions done 
-
 	for contest in all_contests: 
-
 		questionCount = QuizQuestion.objects.filter(contest = contest).count() 
-
 		if questionCount >= 10: 
-
 			contests.append(contest) 
 
 	# Update the context for this page 
-
 	args = {'new_contests' : new_contests, 'active_contests' : active_contests, 
 			'past_contests' : past_contests, 'user_contests' : user_contests, 
 			'contests' : contests, 'new_contests_size' : len(new_contests),
@@ -234,7 +229,6 @@ def schedule_quiz(request):
 			return HttpResponseRedirect('schedule?message={}'.format(message)) 
 
 		# Save contest 
-
 		contest = Contest(host = user, genre = genre, time = date + ' ' + time, valid_for = int(valid_for), time_per_question = int(time_per_question)) 
 		contest.save() 
 		contest_id = contest.id 
@@ -246,7 +240,6 @@ def schedule_quiz(request):
 		return render(request, 'quiz_schedule.html') 
 
 # Method to check if a contest is valid or not 
-
 def is_valid_contest(contest): 
 
 	questions = QuizQuestion.objects.filter(contest = contest) 
@@ -300,7 +293,6 @@ def create_contest(request):
 		else:
 
 			# Finish question 
-			
 			msg = is_valid_contest(contest) 
 
 			if msg == 'OK': 
@@ -322,7 +314,6 @@ def create_contest(request):
 		return render(request, 'quiz_contest_base.html', args) 
 
 # Method to view a contest 
-
 @login_required
 def view_contest(request): 
 
@@ -334,11 +325,8 @@ def view_contest(request):
 	args = {'user' : user, 'contest' : contest, 'questions' : questions}  
 
 	if len(questions) > 0: 
-
 		args['first_question'] = questions[0] 
-
 	else:
-
 		args['first_question'] = 'No questions yet.'
 
 	submissions = Submission.objects.filter(user = user)
@@ -352,15 +340,12 @@ def view_contest(request):
 		if submission.question.contest == contest: 
 
 			# User has played contest 
-
 			flag = True 
-
 			break
 
 	if flag == True:
 
 		# User has played the contest 
-
 		contest_submission = [] 
 		correct_answer = 0 
 		time_taken = 0 
@@ -374,18 +359,21 @@ def view_contest(request):
 				if is_correct(submission, submission.question): 
 
 					# Correct answer 
-
 					time_taken += submission.time_taken
 					correct_answer += 1 
-					contest_submission.append([submission.question.question, submission.answer, submission.question.answer, '1'])
+					correct_subs, total_subs = fetch_question_attempts_accuracy(submission.question)
+					contest_submission.append([submission.question.question, submission.answer, submission.question.answer, '1', 
+					correct_subs, total_subs])
 
 				else: 
 
 					# Incorrect answer 
+					correct_subs, total_subs = fetch_question_attempts_accuracy(submission.question)
+					contest_submission.append([submission.question.question, submission.answer, submission.question.answer, '0',
+					correct_subs, total_subs])
 
-					contest_submission.append([submission.question.question, submission.answer, submission.question.answer, '0'])
-
-		args.update({'played_contest' : 'YES', 'submission' : contest_submission, 'correct_answers' : correct_answer, 'time_taken' : round(time_taken, 3)})
+		args.update({'played_contest' : 'YES', 'submission' : contest_submission, 
+		'correct_answers' : correct_answer, 'time_taken' : round(time_taken, 3)})
 
 	else:
 
@@ -394,13 +382,11 @@ def view_contest(request):
 	if contest.host == user: 
 
 		# Show questions
-
 		args.update({'show_questions' : 'YES'})
 
 	else: 
 
 		# Show contest details only 
-
 		args.update({'show_questions' : 'NO'}) 
 
 	return render(request, 'quiz_view_contest.html', args) 
@@ -411,29 +397,23 @@ def view_contest(request):
 def edit_contest(request): 
 
 	# Fetch request data 
-
 	username = request.GET.get('user') 
 	contest_id = request.GET.get('contest_id') 
 	logged_in_user = request.user 
 
 	# Fetching user from DB 
-
 	user = User.objects.get(username = username) 
 
 	# Check if user is logged in 
-
 	if user != logged_in_user: 
 
 		# Bad request 
-
 		return HttpResponseRedirect('view_contest?contest_id={}&msg={}'.format(contest_id, 'bad_request'))
 
 	# Fetch contest object from DB 
-	
 	contest = Contest.objects.get(id = contest_id) 
 
 	# If contest has been played 
-
 	current_date_time = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))  
 	contest_date_time = contest.time 
 
@@ -444,19 +424,15 @@ def edit_contest(request):
 	if contest_date_time < current_date_time: 
 
 		# Cannot edit it again 
-
 		return HttpResponseRedirect('view_contest?contest_id={}&msg={}'.format(contest_id, 'contest_past')) 
 
 	# Populate context 
-
 	args = {'user' : user, 'contest' : contest}  
 
 	# Proceed to handle request 
-
 	if request.method == 'POST': 
 
 		# Fetch form data 
-
 		genre = request.POST.get('genre') 
 		time = request.POST.get('time') 
 		date = request.POST.get('date') 
@@ -466,18 +442,15 @@ def edit_contest(request):
 		# TODO: Check if timings clash
 
 		# Edit operation 
-
 		contest.genre = genre
 		contest.time = date + ' ' +  time 
 		contest.valid_for = int(valid_for) 
 		contest.time_per_question = request.POST.get('seconds-per-question')
 
 		# Save edits 
-
 		contest.save() 
 
 		# Redirect with acknowledgement 
-
 		return HttpResponseRedirect('view_contest?contest_id={}&msg={}'.format(contest_id, 'edit_success'))
 	
 	else: 
@@ -485,26 +458,21 @@ def edit_contest(request):
 		return render(request, 'quiz_edit_contest.html', args) 
 
 # Method to edit question 
-
 @login_required
 def edit_question(request): 
 
 	# Fetch request data 
-
 	user = request.user 
 	question_id = request.GET.get('question_id') 
 
 	# Fetch data with a DB call 
-
 	question = QuizQuestion.objects.get(id = question_id) 
 	contest = Contest.objects.get(id = question.contest.id) 
 
 	# Defining context for the page 
-
 	args = {'user' : user, 'question' : question, 'contest' : contest}
 
 	# Check if user is author 
-
 	if user != question.contest.host: 
 
 		url = reverse('quiz_home')
@@ -513,7 +481,6 @@ def edit_question(request):
 	if request.method == 'POST': 
 
 		# Check if question is valid 
-
 		question_content = request.POST.get('question') 
 		image = request.FILES.get('image') 		
 		answer = request.POST.get('answer') 
@@ -521,7 +488,6 @@ def edit_question(request):
 		third_answer = request.POST.get('third_answer') 
 
 		# Edit the question
-
 		question.question = question_content 
 		question.answer = answer 
 		question.second_answer = second_answer
@@ -532,7 +498,6 @@ def edit_question(request):
 			question.image = image
 
 		# Save the question 
-
 		question.save() 
 
 		return HttpResponseRedirect('view_contest?contest_id={}'.format(question.contest.id))  
@@ -543,7 +508,6 @@ def edit_question(request):
 
 
 # Method to delete a question 
-
 @login_required
 def delete_question(request): 
 
@@ -551,33 +515,25 @@ def delete_question(request):
 	question_id = request.GET.get('question_id') 
 
 	# Fetch question from database 
-
 	question = QuizQuestion.objects.get(id = question_id) 
 	answer = question.answer 
 
 	# Get contest id 
-
 	contest_id = question.contest.id 
 
 	# Check if user is author 
-
 	if user != question.contest.host: 
 
 		url = reverse('quiz_home')
 		return HttpResponseRedirect(url) 
 
 	# Update context 
-
 	args = {'user' : user, 'question' : question, 'contest_id' : contest_id, 'answer' : answer} 
 
 	if request.method == 'POST': 
-
 		action = request.POST.get('action') 
-
 		if action == 'Confirm delete': 
-
 			# Delete the question  
-
 			question.delete() 
 
 		return HttpResponseRedirect('view_contest?contest_id={}'.format(contest_id)) 
@@ -587,57 +543,46 @@ def delete_question(request):
 		return render(request, 'quiz_delete_question.html', args) 
 
 # Method to fetch next question 
-
 def fetch_next_question(question, user): 
 
 	contest = question.contest 
-
 	questions = QuizQuestion.objects.filter(contest = contest)  
 
-	# Find questions which have already been submitted by user 
-
+	# Find questions which have already been submitted by user
 	for i in range(len(questions)): 
-
-		if i < len(questions) - 1:
 		
+		if i < len(questions) - 1:
 			submission = Submission.objects.filter(user = user, question = questions[i + 1]) 
 
 		if questions[i].id == question.id and len(submission) == 0: 
-
 			break 
 
 	if i == len(questions) - 1: 
 
 		# Contest finished
-
 		message = 'FINISH' 
 		next_question = None
 
 	else: 
 
 		# Contest is on
-
 		message = 'CONTINUE'
 		next_question = questions[i + 1]  
 
 	return message, next_question 
 
 # Method to check whether answer is correct 
-
 def is_correct(submission, question): 
 
 	# Fetching given and correct answers 
-
 	submitted_answer = submission.answer 
 	correct_answer = question.answer 
 
-	# Fetching other accepted answers 
-
+	# Fetching other accepted answers
 	answer1 = question.second_answer 
 	answer2 = question.third_answer 
 
 	# Check with original answer 
-
 	if similarity_quotient(submitted_answer, correct_answer) >= 0.85: 
 
 		return True 
@@ -651,20 +596,16 @@ def is_correct(submission, question):
 		return True 
 
 	# Answer does not match with anything 
-
 	return False 
 
 # Method to return similarity of two strings 
-
 def similarity_quotient(str1, str2): 
 
 	# Convert everything to lower case 
-
 	str1 = str1.lower()
 	str2 = str2.lower() 
 
 	# Compute similarity
-
 	similarity = difflib.SequenceMatcher(None, str1, str2).ratio() 
 
 	print(str1, str2, similarity) 
@@ -675,69 +616,56 @@ def similarity_quotient(str1, str2):
 def play_contest(request): 
 
 	# Fetch data from request 
-
 	user = request.user 
 	question_id = request.GET.get('question_id')
 	contest_id = request.GET.get('contest_id') 
 	instruction = request.GET.get('instruction') 
 
 	# Fetch data from database call 
-
 	contest = Contest.objects.get(id = contest_id) 
 	question = QuizQuestion.objects.get(id = question_id) 
 	submissions = Submission.objects.filter(user = user, question = question) 
 	contest_questions = QuizQuestion.objects.filter(contest = contest) 
 
 	# TODO: If it has been ten minutes since contest started - leave 
-	
 	if len(submissions) == len(contest_questions): 
 
 		return redirect('quiz_home') 
 
-	index = len(submissions) 
-
+	index = len(submissions)
 	question = contest_questions[index] 
-
 	args = {'user' : user, 'question' : question, 'contest' : contest} 
 
 	if request.method == 'POST': 
 
 		# Fetch request data 
-
 		answer = request.POST.get('answer') 
 		question_id = request.GET.get('question_id') 
 		time_taken = request.POST.get('time_taken') 
 
 		# Pre-process incoming data
-
 		if answer == None: 
-
 			answer = "" 
 
 		if time_taken == None: 
-
 			time_taken = 0
 
 		time_taken = int(time_taken) / 1000
 
 		# Fetch question 
-
 		question = QuizQuestion.objects.get(id = question_id) 
 
 		# Check if user has submitted this question before 
-
 		submissions = Submission.objects.filter(user = user, question = question) 
 
 		if len(submissions) == 0:
 
 			# Create submission object 
-
 			submission = Submission(user = user, question = question, 
 			answer = answer, time_taken = time_taken) 
 			submission.save() 
 
 		# Route to next question 
-
 		message, next_question = fetch_next_question(question, user) 
 
 		if message == 'FINISH': 
@@ -762,7 +690,6 @@ def play_contest(request):
 			args['contest'] = contest 
 			
 			questions = QuizQuestion.objects.filter(contest = contest_id)
-
 			args['no_of_questions'] = len(questions)
 
 			return render(request, 'quiz_contest_instructions.html', args) 
@@ -771,8 +698,7 @@ def play_contest(request):
 
 			return render(request, 'quiz_play_contest.html', args) 
 
-# Method to return a list of users which participated in the contest 
-	
+# Method to return a list of users which participated in the contest	
 def users_in_contest(contest): 
 
 	users_set = set([]) 
@@ -1167,3 +1093,22 @@ def restore_ratings(request):
 	else:
 
 		return display_leaderboard(request) 
+
+
+# Method to fetch question data (how many questions answered) 
+def fetch_question_attempts_accuracy(question): 
+
+	# From a question object fetch how many people answered it out of total [returning a %age]
+	
+	# Fetching all submissions 
+	submission_list = list(Submission.objects.filter(question = question)) 
+
+	# Checking how many correct
+	correct_subs = 0
+	total_subs   = len(submission_list)
+
+	for submission in submission_list: 
+		if is_correct(submission, submission.question): 
+			correct_subs += 1
+
+	return correct_subs, total_subs
